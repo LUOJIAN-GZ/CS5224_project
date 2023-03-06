66943d825a51ea1437390c1a2ed9827c0d8e06ac
TONGW0226 <tamiatong0226@gmail.com>
2023-03-06 19:18:56 +0800

    Added map_page.dart from previous main.dart

--- my_awesome_namer/lib/pages/map_page.dart
+++ my_awesome_namer/lib/pages/map_page.dart
@@ -1,15 +1,58 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import '../src/locations.dart' as locations;
 
 class MapPage extends StatefulWidget {
-  const MapPage({super.key});
+  const MapPage({Key? key}) : super(key: key);
 
   @override
   State<MapPage> createState() => _MapPageState();
 }
 
 class _MapPageState extends State<MapPage> {
+  final Map<String, Marker> _markers = {};
+  Future<void> _onMapCreated(GoogleMapController controller) async {
+    final googleOffices = await locations.getGoogleOffices();
+    setState(() {
+      _markers.clear();
+      for (final office in googleOffices.offices) {
+        final marker = Marker(
+          markerId: MarkerId(office.name),
+          position: LatLng(office.lat, office.lng),
+          infoWindow: InfoWindow(
+            title: office.name,
+            snippet: office.address,
+          ),
+        );
+        _markers[office.name] = marker;
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Center(
+      child: Container(
+        height: 0.8 * MediaQuery.of(context).size.height,
+        width: 0.9 * MediaQuery.of(context).size.width,
+        decoration: const BoxDecoration(
+          // color: Colors.white,
+          boxShadow: <BoxShadow>[
+            BoxShadow(
+                color: Color.fromRGBO(0, 0, 0, 0.6),
+                offset: Offset(10, 10),
+                blurRadius: 10),
+          ],
+        ),
+        child: GoogleMap(
+          onMapCreated: _onMapCreated,
+          initialCameraPosition: const CameraPosition(
+            target: LatLng(1.3521, 103.8198),
+            zoom: 12.0,
+          ),
+          markers: _markers.values.toSet(),
+        ),
+      ),
+    );
   }
 }
